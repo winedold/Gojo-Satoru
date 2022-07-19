76dfab12aae2e535ae7a81744a16fac96ccba6b8
winedold <109556915+winedold@users.noreply.github.com>
2022-07-19 15:35:52 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.owner = ['3501325207']
+global.premium = ['']
+global.ownernomer = ''
+global.ownername = 'wined'
+global.botname = 'babayaga'
+global.footer = ''
+global.ig = ''
+global.region = 'Italy'
+global.sc = ''
+global.myweb = ''
+global.packname = 'babayaga'
+global.author = ''
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
